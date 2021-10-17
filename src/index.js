3d7f7bb414a167c75c6b47ced9ed18b4d5ebe997
Soocyang <36953143+Soocyang@users.noreply.github.com>
2021-10-17 16:13:05 +0800

    update help prefix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const { Client, Intents } = require("discord.js");
-const WOKCommands = require("wokcommands");
-const path = require("path");
-const MusicService = require("./service/MusicService.js");
+require('dotenv').config()
+const { Client, Intents } = require('discord.js')
+const WOKCommands = require('wokcommands')
+const path = require('path')
+const MusicService = require('./service/MusicService.js')
 
 const client = new Client({
 	// intents
@@ -17,23 +17,23 @@ const client = new Client({
 		Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
 	],
 	// Use recommended partials for the built-in help menu
-	partials: ["MESSAGE", "REACTION"],
-});
+	partials: ['MESSAGE', 'REACTION'],
+})
 
-let musicPlayer = new MusicService();
-let botPrefix = "--";
+let musicPlayer = new MusicService()
+let botPrefix = '--'
 
 module.exports = {
 	musicPlayer: musicPlayer,
 	botPrefix: botPrefix,
-};
+}
 
-client.once("ready", async () => {
-	console.log("DJ Murrr is Online!");
-	client.user.setActivity(`=help`, { type: "PLAYING" });
+client.once('ready', async () => {
+	console.log('DJ Murrr is Online!')
+	client.user.setActivity(`${botPrefix}help`, { type: 'PLAYING' })
 
 	new WOKCommands(client, {
-		commandsDir: path.join(__dirname, "commands"),
+		commandsDir: path.join(__dirname, 'commands'),
 		testServers: [process.env.GUILD_ID],
 		showWarns: false,
 	})
@@ -41,10 +41,10 @@ client.once("ready", async () => {
 		.setBotOwner(process.env.MY_USER_ID)
 		.setCategorySettings([
 			{
-				name: "Music",
-				emoji: "🎵",
+				name: 'Music',
+				emoji: '🎵',
 			},
-		]);
-});
+		])
+})
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
